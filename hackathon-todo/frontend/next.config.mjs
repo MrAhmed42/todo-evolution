dc94d4f0963b1552fd4c1abfba40a51ff0652034
MrAhmed42 <ahmednadeem0004@gmail.com>
2026-02-05 21:55:53 +0500

    feat(todo1): integrate Phase II & III full-stack app with AI agents and MCP
    Successfully implemented

--- hackathon-todo/frontend/next.config.mjs
+++ hackathon-todo/frontend/next.config.mjs
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
+        // This proxies your frontend calls to your Hugging Face Backend
         source: "/api/:path*",
         destination: "https://ahmed421092-hack-2-phrase-3.hf.space/api/:path*",
       },
